9fe9c46416c97bb65928f0a0663a13305f5221f2
Knightmare-sama <51862748+Knightmare-sama@users.noreply.github.com>
2020-11-03 21:04:58 +0530

    been there done dat

---
+++ Basic Ball/Assets/Ball.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        if(isGrounded)
+        rb.AddForce(new Vector3(0f, 15f, 0f),ForceMode.Impulse);
+    }
+
+    public void MoveLeft()
+    {
+        if (isGrounded)
+        {
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 15f);
+        //rb.AddForce(new Vector3(0f, 0f, 500f));
+        }
+
+    }
+
+    public void MoveRight()
+    {
+        if (isGrounded)
+        {
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -15f);
+        //rb.AddForce(new Vector3(0f, 0f, -500f));
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
---
+++ Basic Ball/Assets/CamFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 offset;
+    [SerializeField] float smoothSpeed;
+
+    [SerializeField] bool lookAt;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desiredPos = target.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPos;
+
+        if(lookAt)
+            transform.LookAt(target);
+    }
+}
